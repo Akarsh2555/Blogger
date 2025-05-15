cb61f006cdfb30a360595387bf00b4398b16a075
Akarsh Chaturvedi <akarshchaturvedi2555@gmail.com>
2025-05-15 16:16:37 +0530

    Your commit message here

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -27,7 +27,7 @@ export default function Home() {
                 <Container>
                     <div className="flex flex-wrap w-full h-screen">
                         <div className="flex w-full h-60 justify-between items-center px-4">
-                            <img src="public/img3.jpg" className="w-1/6 h-auto animate-slide-down" />
+                            <img src="/img3.jpg" className="w-1/6 h-auto animate-slide-down" />
                             <div>
                                 <h1 className="text-5xl text-white"> Create Your Passion Your Way</h1>
                                 <h2 className="text-left text-2xl text-white text-center">Create Unique and beautiful posts </h2>
@@ -38,7 +38,7 @@ export default function Home() {
                                     Create Post Now
                                 </button>
                             </div>
-                            <img src="../public/img4.jpg" className="w-1/6 h-auto animate-slide-down " />
+                            <img src="/img4.jpg" className="w-1/6 h-auto animate-slide-down " />
                         </div>
                         <div className="p-2 w-full flex justify-center items-center ">
                             <img src="https://blogger.googleusercontent.com/img/b/U2hvZWJveA/AVvXsEhw5K6-sy8Pgp7Yply7EXGI9Tqy6t-0ojmL_JZbdyIHjgthYQnpxqUesl5wZfpbiMwHN_x3M6CAEiND5MqGsHE1MEUOSvyXn3PHFWv8Cw_NlPzQEkPnYwS61RGHVmGXXwvTNcgBV0VMz-U2ecMArpMVGrwVaYXl4Fo/w192" className="w-1/4 h-auto animate-slide-down mt-auto"
@@ -53,27 +53,27 @@ export default function Home() {
                         className=" w-full h-80 object-cover"
                         src="https://tse3.mm.bing.net/th/id/OIP.sd2nOUY1dyJbehNlJ054RgHaEK?rs=1&pid=ImgDetMain" />
                     <div className="bg-[#388D80] py-8  mt-4 text-center text-white flex group ">
-                        <img src="public/filler2.jpg" className="w-1/3 h-auto a group-hover:animate-zoom-in" />
+                        <img src="/filler2.jpg" className="w-1/3 h-auto a group-hover:animate-zoom-in" />
                         <div className="text-left ml-20 text-center ">
                             <h1 className="text-5xl mb-5  mt-20"> Hang onto your Memories</h1>
                             <h2 className="text-left text-2xl">Save the moments that matter. Blogger lets you safely store </h2>
                             <h2 className="text-2xl">thousands of posts, photos!</h2>
-                            <img src="public/filler4.jpg" className="group-hover:animate-bounce-in" />
-                            <img src="public/filler5.jpg" />
+                            <img src="/filler4.jpg" className="group-hover:animate-bounce-in" />
+                            <img src="/filler5.jpg" />
                         </div>
-                        <img src="public/filler3.jpg" />
+                        <img src="/filler3.jpg" />
                     </div>
                     <div className="bg-[#4583AA] py-8 mt-4 text-center text-white flex group ">
-                        <img src="public/img6.jpg" className="w-1/5 h-auto a group-hover:animate-zoom-in" />
+                        <img src="/img6.jpg" className="w-1/5 h-auto a group-hover:animate-zoom-in" />
                         <div className="text-left ml-20 text-center ">
                             <h1 className="text-5xl mb-5  mt-20"> Choose the perfect designs</h1>
                             <h2 className="text-left text-2xl">Create a beautiful blog that fits your style. Choose from a selection of easy-to-use templates – all with flexible layouts and hundreds of background images – or design something new.</h2>
                             <div className="flex justify-between">
-                                <img src="public/img5.jpg" className="group-hover:animate-bounce-in" />
-                                <img src="public/img8.jpg" className="group-hover:animate-bounce-in mr-20" />
+                                <img src="/img5.jpg" className="group-hover:animate-bounce-in" />
+                                <img src="/img8.jpg" className="group-hover:animate-bounce-in mr-20" />
                             </div>
                         </div>
-                        <img src="public/img7.jpg" />
+                        <img src="/img7.jpg" />
                     </div>
                 </Container>
             </div>
